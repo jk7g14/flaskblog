9a05807140f178df5924e433fad5e3c3d6258837
jk7g14 <jk7g14@gmail.com>
2019-08-01 01:53:18 +0900

    SQLALCHEMY_TRACK_MODIFICATIONS

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,6 +5,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config['SECRET_KEY']= '688be7a05a56f53221054a7dfb489d70'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
